cf4657223befa0cf7d3ed8c6bbedb003583cc02c
HoffmannV <vladimir.hoffmann@outlook.de>
2024-10-24 09:58:41 +0200

    Added a TextNode Class and a Enum for the TextTypes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("hello world")
+from textnode import TextType, TextNode
+
+def main():
+    node = TextNode("text", TextType.bold_text)
+    node2 = TextNode("text", TextType.bold_text)
+    boot_dev_node = TextNode("This is a text node", TextType.bold_text, "https://www.boot.dev")
+
+    print(boot_dev_node)
+    print(node)
+    print(boot_dev_node == node)
+    print(node == node2)
+
+if __name__ == "__main__":    
+    main()
